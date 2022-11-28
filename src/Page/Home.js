be756fffc6cb62548dfc72497ef2b4f8a23d2360
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2022-11-28 10:38:18 -0300

    Adicionando o projeto
    Este todo-list já estava pronto em um outro projeto, percebi que no projeto inicial não combinava o todo-list. Então resolvi separar ele do projeto inicial.

---
+++ src/Page/Home.js
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+
+import "./home.css";
+
+import {
+  AiFillDelete,
+  AiFillPlusCircle,
+  AiOutlineCheckCircle,
+} from "react-icons/ai";
+
+export default function Home() {
+  const [texts, setTexts] = useState("");
+  const [id, setId] = useState(0);
+
+  const [arrayTodo, setArrayTodo] = useState(
+    () => JSON.parse(localStorage.getItem("arrayTodo")) || []
+  );
+
+  useEffect(() => {
+    //soluçao para o problema no id
+    //quando a pagina recarregava o id voltava a 0, então se o usuario recarregasse a pagina e adicionasse mais 1 item na lista, esse item viria com o id 0, fazendo com q o id se repetisse
+
+    let storedArray = JSON.parse(localStorage.getItem("arrayTodo")) || [];
+    let getId = storedArray.map((task) => {
+      return task.id;
+    });
+
+    let lastId = getId[getId.length - 1];
+
+    setId(lastId + 1 || 0);
+  }, []);
+
+  function addTodo() {
+    if (texts.length < 1) {
+      alert("Digite alguma coisa ");
+      return;
+    }
+
+    const todoObj = {
+      id: id,
+      text: texts,
+      checkedButton: true,
+      valor: 0,
+      quant: 1,
+    };
+    setId(id + 1);
+
+    setArrayTodo([...arrayTodo, todoObj]);
+
+    setTexts("");
+  }
+
+  useEffect(() => {
+    localStorage.setItem("arrayTodo", JSON.stringify(arrayTodo));
+  }, [arrayTodo]);
+
+  function deleteItem(id) {
+    let filtered = arrayTodo.filter((todo) => todo.id !== id);
+    setArrayTodo(filtered);
+
+    localStorage.setItem("arrayTodo", JSON.stringify(filtered));
+    console.log(arrayTodo);
+  }
+
+  return (
+    <div className="containerHome">
+      <h1 className="titleNotes">ANOTAÇÕES</h1>
+      <div className="inputButton">
+        <input
+          type="text"
+          maxLength="40"
+          placeholder="Digite aqui..."
+          value={texts}
+          onChange={(e) => setTexts(e.target.value)}
+          autoFocus
+        />
+        <button onClick={addTodo}>
+          <AiFillPlusCircle />
+        </button>
+      </div>
+
+      <div className="containerLista">
+        {/* <h1 className="titleNotes">ANOTAÇÕES</h1> */}
+
+        {arrayTodo.map((item, index) => (
+          <div key={index} className="lista">
+            <span>
+              <button>
+                <AiOutlineCheckCircle />
+              </button>
+              {item.text}
+            </span>
+            <button
+              className="buttonDelete"
+              onClick={() => deleteItem(item.id)}
+            >
+              <AiFillDelete />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
